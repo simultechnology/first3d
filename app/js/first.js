8102ca4bdc047b4b90f40b2d791e51d7a7e3e6f9
takatsugu ishikawa <tissi0708@gmail.com>
2014-08-10 22:59:23 +0900

    Initial commit

---
+++ app/js/first.js
@@ -0,0 +1,10 @@
+(function () {
+
+  // scene
+  var scnen = new THREE.Scene();
+
+  // mesh
+  var geometry = new THREE.CubeGeometry(50, 50, 50);
+  var material = new THREE.MeshBasicMaterial
+
+}());
