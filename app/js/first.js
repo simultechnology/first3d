5bc9b43ef2908da42994eca0ee1b3afd96692a85
takatsugu ishikawa <tissi0708@gmail.com>
2014-08-10 23:37:11 +0900

    rendering object

--- app/js/first.js
+++ app/js/first.js
@@ -1,10 +1,31 @@
 (function () {
 
+  var width = 500, height = 300;
+
   // scene
-  var scnen = new THREE.Scene();
+  var scene = new THREE.Scene();
 
   // mesh
   var geometry = new THREE.CubeGeometry(50, 50, 50);
-  var material = new THREE.MeshBasicMaterial
+  var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+  var cube = new THREE.Mesh(geometry, material);
+  cube.position.set(0, 0, 0);
+  scene.add(cube);
+
+  // light
+
+  // camera
+  var camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.set(200, 100, 500);
+  //camera.position.x = 0;
+  //camera.position = new THREE.Vector3(200, 100, 500);
+  camera.lookAt(cube.position);
+
+  // rendering
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xeeeeee, 1);
+  document.getElementById('stage').appendChild(renderer.domElement);
+  renderer.render(scene, camera);
 
 }());
