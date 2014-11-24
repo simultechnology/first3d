4d827f887754172e6e910110c8569744441f5e1a
takatsugu ishikawa <tissi0708@gmail.com>
2014-11-24 20:04:40 +0900

    add Shadow

--- app/js/first.js
+++ app/js/first.js
@@ -10,19 +10,32 @@
   //var material = new THREE.MeshBasicMaterial({color: 0xff0000});
   var material = new THREE.MeshLambertMaterial({color: 0xff0000});
   var cube = new THREE.Mesh(geometry, material);
-  cube.position.set(0, 0, 0);
+  cube.position.set(0, 50, 0);
+  cube.castShadow = true;
   scene.add(cube);
 
-  var sGeometry = new THREE.SphereGeometry(30);
+  //var sGeometry = new THREE.SphereGeometry(30);
+  ////var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+  //var sphMaterial = new THREE.MeshLambertMaterial({color: 0x121212});
+  //var sphere = new THREE.Mesh(sGeometry, sphMaterial);
+  //sphere.position.set(100, 100, 100);
+  //scene.add(sphere);
+
+  var pGeometry = new THREE.PlaneGeometry(300, 300);
   //var material = new THREE.MeshBasicMaterial({color: 0xff0000});
-  var sphMaterial = new THREE.MeshLambertMaterial({color: 0x121212});
-  var sphere = new THREE.Mesh(sGeometry, sphMaterial);
-  sphere.position.set(100, 100, 100);
-  scene.add(sphere);
+  var pMaterial = new THREE.MeshLambertMaterial({
+    color: 0x0096d6,
+    side: THREE.DoubleSide
+  });
+  var plane = new THREE.Mesh(pGeometry, pMaterial);
+  plane.receiveShadow = true;
+  plane.position.set(0, 0, 0);
+  plane.rotation.x = 90 * Math.PI / 180; // radian
+  scene.add(plane);
 
   // camera
   var camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
-  camera.position.set(200, 100, 550);
+  camera.position.set(200, 300, 550);
   //camera.position.x = 0;
   //camera.position = new THREE.Vector3(200, 100, 500);
   camera.lookAt(cube.position);
@@ -35,6 +48,7 @@
   // light
   var light = new THREE.DirectionalLight(0xffffff, 1);
   light.position.set(0, 100, 30);
+  light.castShadow = true;
   scene.add(light);
   var ambient = new THREE.AmbientLight(0x5500000);
   scene.add(ambient);
@@ -43,6 +57,7 @@
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize(width, height);
   renderer.setClearColor(0xeeeeee, 1);
+  renderer.shadowMapEnabled = true;
   document.getElementById('stage').appendChild(renderer.domElement);
   renderer.render(scene, camera);
 
